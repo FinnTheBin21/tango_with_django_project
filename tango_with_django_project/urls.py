7a23d9ff03e29e43965986cbf12aed2ca8d1722a
Finn McInroy (student) <2774174M@student.gla.ac.uk>
2025-02-07 15:57:58 +0000

    chapter 3 done

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -20,6 +20,7 @@ from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
     path('rango/', include('rango.urls')),
     #the above maps any urls starting with rango/ to be handled by rango
     path('admin/', admin.site.urls),
